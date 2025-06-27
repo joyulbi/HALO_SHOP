3cfdddc1b20eb0147ec10817a46fd21b2c2a289e
DESKTOP-FQC751U\tj-bu-703-14 <urby0725@naver.com>
2025-06-27 10:46:06 +0900

    프로젝트 시작 2025-06-27

---
+++ haloshop/src/main/java/com/company/haloshop/HaloShopApplication.java
@@ -0,0 +1,13 @@
+package com.company.haloshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaloShopApplication.class, args);
+	}
+
+}
---
+++ haloshop/src/test/java/com/company/haloshop/HaloShopApplicationTests.java
@@ -0,0 +1,14 @@
+package com.company.haloshop;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaloShopApplicationTests {
+	   @Test
+	    void contextLoads() {
+	        //테스트용 지워버려도됨
+	    }
+	
+}
