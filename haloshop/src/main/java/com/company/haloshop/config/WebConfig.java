371832a21cd12c2820fdfe6d608902fbf2fc1043
jaewon <wodnjs0610@naver.com>
2025-06-27 18:21:02 +0900

    2025-06-27 리액트 스프링 연결

--- haloshop/src/main/java/com/company/haloshop/config/WebConfig.java
+++ haloshop/src/main/java/com/company/haloshop/config/WebConfig.java
@@ -7,11 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // React에서 호출하는 API 경로
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/api/**")
+	            .allowedOrigins("http://localhost:3000") // 프론트 포트 잘 적었음
+	            .allowedMethods("GET", "POST", "PUT", "DELETE")
+	            .allowedHeaders("*") // 혹시 헤더 제한 생기지 않게 전체 허용 추가
+	            .allowCredentials(true);
+	}
 }
