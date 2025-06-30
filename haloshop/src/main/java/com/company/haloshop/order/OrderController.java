21a8975764462530d50e4918a71074b4db27a0d4
DESKTOP-FQC751U\tj-bu-703-14 <urby0725@naver.com>
2025-06-30 10:40:59 +0900

    오류수정

--- haloshop/src/main/java/com/company/haloshop/order/OrderController.java
+++ haloshop/src/main/java/com/company/haloshop/order/OrderController.java
@@ -1,11 +1,21 @@
 package com.company.haloshop.order;
 
-import com.company.haloshop.dto.shop.OrderDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.haloshop.dto.shop.OrderDto;
+import com.company.haloshop.dto.shop.OrderRequestDto;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -24,11 +34,17 @@ public class OrderController {
         return ResponseEntity.ok(orderService.findById(id));
     }
 
-    @PostMapping
+    /*@PostMapping
     public ResponseEntity<Void> insert(@RequestBody OrderDto orderDto) {
         orderService.insert(orderDto);
         return ResponseEntity.ok().build();
+    }*/
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody OrderRequestDto orderRequestDto) {
+        orderService.insertOrderWithItems(orderRequestDto);
+        return ResponseEntity.ok().build();
     }
+
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody OrderDto orderDto) {
